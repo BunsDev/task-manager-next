20db651a733499af265b54d35bbdb6797366ccab
Kartik <kartik20044@gmail.com>
2024-08-23 08:07:06 -0300

    revalidate on delete added

--- src/actions/delete-project.ts
+++ src/actions/delete-project.ts
@@ -1,13 +1,16 @@
-"use server"
+"use server";
 
-import prisma from "@/db/prisma"
+import prisma from "@/db/prisma";
+import { revalidatePath } from "next/cache";
 
-export async function deleteProject(projectId:string) {
+export async function deleteProject(projectId: string) {
     try {
         await prisma.project.delete({
-            where:{id:projectId}
-        })
+            where: { id: projectId },
+        });
     } catch (error) {
-        throw new Error("Unable to delete Project")
+        throw new Error("Unable to delete Project");
+    } finally {
+         revalidatePath("/project");
     }
 }
